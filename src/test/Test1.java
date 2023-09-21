b039a873228a225613ba7b642cf16bb50330bb64
gpffhdn963 <gpffhdn963@naver.com>
2023-09-21 12:42:24 +0900

    팀장수정

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,5 +3,10 @@ package test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
